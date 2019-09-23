53963e0c203e270722baf5dfa061e5e212da8c92
Shaleen Jain <shaleen@jain.sh>
2019-09-23 14:44:23 +0530

    add rustwasm tests

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,13 @@ use fixedbitset::FixedBitSet;
 use std::fmt;
 use wasm_bindgen::prelude::*;
 
+// A macro to provide `println!(..)`-style syntax for `console.log` logging.
+macro_rules! log {
+    ( $( $t:tt )* ) => {
+        web_sys::console::log_1(&format!( $( $t )* ).into());
+    }
+}
+
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
 #[cfg(feature = "wee_alloc")]
@@ -39,6 +46,20 @@ impl Universe {
 
         count
     }
+
+    /// Get the dead and alive values of the entire universe.
+    pub fn get_cells(&self) -> &[u32] {
+        &self.cells.as_slice()
+    }
+
+    /// Set cells to be alive in a universe by passing the row and column
+    /// of each cell as an array.
+    pub fn set_cells(&mut self, cells: &[(u32, u32)]) {
+        for (row, col) in cells.iter().cloned() {
+            let idx = self.get_index(row, col);
+            self.cells.set(idx, true);
+        }
+    }
 }
 
 #[wasm_bindgen]
@@ -52,9 +73,17 @@ impl Universe {
                 let cell = self.cells[idx];
                 let live_neighbors = self.live_neighbor_count(row, col);
 
-                next.set(
-                    idx,
-                    match (cell, live_neighbors) {
+                /*
+                log!(
+                    "cell[{}, {}] is initially {:?} and has {} live neighbors",
+                    row,
+                    col,
+                    cell,
+                    live_neighbors
+                );
+                */
+
+                let next_cell = match (cell, live_neighbors) {
                         // Rule 1: Any live cell with fewer than two live neighbours
                         // dies, as if caused by underpopulation.
                         (true, x) if x < 2 => false,
@@ -69,8 +98,10 @@ impl Universe {
                         (false, 3) => true,
                         // All other cells remain in the same state.
                         (otherwise, _) => otherwise,
-                    },
-                );
+                    };
+
+               // log!("    it becomes {:?}", next_cell);
+                next.set(idx, next_cell);
             }
         }
 
@@ -108,6 +139,22 @@ impl Universe {
 
     pub fn cells(&self) -> *const u32 {
         self.cells.as_slice().as_ptr()
+    }
+
+    /// Set the width of the universe.
+    ///
+    /// Resets all cells to the dead state.
+    pub fn set_width(&mut self, width: u32) {
+        self.width = width;
+        self.cells.clear();
+    }
+
+    /// Set the height of the universe.
+    ///
+    /// Resets all cells to the dead state.
+    pub fn set_height(&mut self, height: u32) {
+        self.height = height;
+        self.cells.clear();
     }
 }
 
