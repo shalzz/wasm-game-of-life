328bd86d06391fd002da1a73c5f563d674d8fca9
Shaleen Jain <shaleen@jain.sh>
2019-11-14 17:39:49 +0530

    wasm: Implement back buffer instead of cloning
    Use a double buffer technique where we update the back buffer
    and swap it to the front buffer on every frame update.
    
    This prevents use from allocating a new buffer on every frame as
    now we are only every allocating two buffer through out the program run.

--- src/lib.rs
+++ src/lib.rs
@@ -15,6 +15,7 @@ pub struct Universe {
     width: u32,
     height: u32,
     cells: FixedBitSet,
+    back_cells: FixedBitSet,
 }
 
 impl Universe {
@@ -24,18 +25,11 @@ impl Universe {
 
     fn live_neighbor_count(&self, row: u32, column: u32) -> u8 {
         let mut count = 0;
+        let cells = &self.cells;
 
-        let north = if row == 0 {
-            self.height - 1
-        } else {
-            row - 1
-        };
+        let north = if row == 0 { self.height - 1 } else { row - 1 };
 
-        let south = if row == self.height - 1 {
-            0
-        } else {
-            row + 1
-        };
+        let south = if row == self.height - 1 { 0 } else { row + 1 };
 
         let west = if column == 0 {
             self.width - 1
@@ -50,35 +44,35 @@ impl Universe {
         };
 
         let nw = self.get_index(north, west);
-        count += self.cells[nw] as u8;
+        count += cells[nw] as u8;
 
         let n = self.get_index(north, column);
-        count += self.cells[n] as u8;
+        count += cells[n] as u8;
 
         let ne = self.get_index(north, east);
-        count += self.cells[ne] as u8;
+        count += cells[ne] as u8;
 
         let w = self.get_index(row, west);
-        count += self.cells[w] as u8;
+        count += cells[w] as u8;
 
         let e = self.get_index(row, east);
-        count += self.cells[e] as u8;
+        count += cells[e] as u8;
 
         let sw = self.get_index(south, west);
-        count += self.cells[sw] as u8;
+        count += cells[sw] as u8;
 
         let s = self.get_index(south, column);
-        count += self.cells[s] as u8;
+        count += cells[s] as u8;
 
         let se = self.get_index(south, east);
-        count += self.cells[se] as u8;
+        count += cells[se] as u8;
 
         count
     }
 
     /// Get the dead and alive values of the entire universe.
-    pub fn get_cells(&self) -> &[u32] {
-        &self.cells.as_slice()
+    pub fn get_cells(&self) -> *const u32 {
+        self.cells.as_slice().as_ptr()
     }
 
     /// Set cells to be alive in a universe by passing the row and column
@@ -94,8 +88,6 @@ impl Universe {
 #[wasm_bindgen]
 impl Universe {
     pub fn tick(&mut self) {
-        let mut next = self.cells.clone();
-
         for row in 0..self.height {
             for col in 0..self.width {
                 let idx = self.get_index(row, col);
@@ -103,27 +95,27 @@ impl Universe {
                 let live_neighbors = self.live_neighbor_count(row, col);
 
                 let next_cell = match (cell, live_neighbors) {
-                        // Rule 1: Any live cell with fewer than two live neighbours
-                        // dies, as if caused by underpopulation.
-                        (true, x) if x < 2 => false,
-                        // Rule 2: Any live cell with two or three live neighbours
-                        // lives on to the next generation.
-                        (true, 2) | (true, 3) => true,
-                        // Rule 3: Any live cell with more than three live
-                        // neighbours dies, as if by overpopulation.
-                        (true, x) if x > 3 => false,
-                        // Rule 4: Any dead cell with exactly three live neighbours
-                        // becomes a live cell, as if by reproduction.
-                        (false, 3) => true,
-                        // All other cells remain in the same state.
-                        (otherwise, _) => otherwise,
-                    };
+                    // Rule 1: Any live cell with fewer than two live neighbours
+                    // dies, as if caused by underpopulation.
+                    (true, x) if x < 2 => false,
+                    // Rule 2: Any live cell with two or three live neighbours
+                    // lives on to the next generation.
+                    (true, 2) | (true, 3) => true,
+                    // Rule 3: Any live cell with more than three live
+                    // neighbours dies, as if by overpopulation.
+                    (true, x) if x > 3 => false,
+                    // Rule 4: Any dead cell with exactly three live neighbours
+                    // becomes a live cell, as if by reproduction.
+                    (false, 3) => true,
+                    // All other cells remain in the same state.
+                    (otherwise, _) => otherwise,
+                };
 
-                next.set(idx, next_cell);
+                self.back_cells.set(idx, next_cell);
             }
         }
 
-        self.cells = next;
+        std::mem::swap(&mut self.cells, &mut self.back_cells);
     }
 
     pub fn new() -> Universe {
@@ -136,11 +128,13 @@ impl Universe {
         for i in 0..size {
             cells.set(i, i % 2 == 0 || i % 7 == 0);
         }
+        let back_cells = cells.clone();
 
         Universe {
             width,
             height,
-            cells,
+            cells: cells,
+            back_cells: back_cells,
         }
     }
 
@@ -184,10 +178,11 @@ impl Universe {
 
 impl fmt::Display for Universe {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let cells = &self.cells;
         for row in 0..self.height {
             for col in 0..self.width {
                 let i = self.get_index(row, col);
-                let symbol = if self.cells[i] { '◻' } else { '◼' };
+                let symbol = if cells[i] { '◻' } else { '◼' };
                 write!(f, "{}", symbol)?;
             }
             write!(f, "\n")?;
